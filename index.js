c67fd4e0f834bbe9d27a4ec0a9c810319dfd117a
Tony Russo <tony@tortugacoders.com>
2019-04-15 12:27:15 +0700

    basic route

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 
 const app = express()
 
+// code here
+
+app.get('/', function(req, res, next) {
+	// res.send('Hello from the server')
+	res.send('<h1>Hello from the server</h1>')
+})
+
 app.listen(3000, function() {
 	console.log('Server ready on port 3000');
 })
