d757ee65ec709ae6c47b696c2febf1ce8e44694d
Tony Russo <tony@tortugacoders.com>
2019-04-15 12:51:05 +0700

    res html page and download file

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ app.get('/', function(req, res, next) {
 	res.send('<h1>Hello from the server</h1>')
 })
 
+app.get('/about', function(req, res, next) {
+	res.send('<h1>About us</h1>')
+})
+
+app.get('/contact', function(req, res, next) {
+	res.sendFile(__dirname + '/contact.html')
+})
+
+app.get('/file', function(req, res, next) {
+	res.download(__dirname + '/notes.txt')
+})
+
 app.listen(3000, function() {
 	console.log('Server ready on port 3000');
 })
