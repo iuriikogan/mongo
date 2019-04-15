f7ec379b637457818565b4591664267f45e012ea
Tony Russo <tony@tortugacoders.com>
2019-04-15 16:33:05 +0700

    channels

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const body_parser = require('body-parser')
 
 const app = express()
+
+// middleware
+
+app.use(body_parser.json())
 
 // code here
 
@@ -19,6 +24,64 @@ app.get('/contact', function(req, res, next) {
 
 app.get('/file', function(req, res, next) {
 	res.download(__dirname + '/notes.txt')
+})
+
+app.get('/channels', function(req, res, next) {
+	let channels = [{
+		id: 1,
+		name: 'general'
+	}, {
+		id: 2,
+		name: 'random'
+	}, {
+		id: 3,
+		name: 'private'
+	}, {
+		id: 4,
+		name: 'fun'
+	}]
+	res.json(channels)
+})
+
+app.get('/channels/:id', function(req, res, next) {
+	console.log('You\'re looking for channel: ' + req.params.id);
+	let channels = [{
+		id: 1,
+		name: 'general'
+	}, {
+		id: 2,
+		name: 'random'
+	}, {
+		id: 3,
+		name: 'private'
+	}, {
+		id: 4,
+		name: 'fun'
+	}]
+	let channel = channels.find(function(c) {
+		return c.id == req.params.id
+	})
+	res.json(channel)
+})
+
+app.post('/channels', function(req, res, next) {
+	console.log('Thank you for your POST request at /channels');
+	console.log('The new channel is' + req.body);
+	let channels = [{
+		id: 1,
+		name: 'general'
+	}, {
+		id: 2,
+		name: 'random'
+	}, {
+		id: 3,
+		name: 'private'
+	}, {
+		id: 4,
+		name: 'fun'
+	}]
+	channels.push(req.body)
+	res.json(channels)
 })
 
 app.listen(3000, function() {
