c6a4e0463add86fc4c473162b38cf972cffdef73
Iurii Kogan <koganiurii@gmail.com>
2019-04-17 14:54:16 +0700

    .populate in messages_read

--- controllers/messages_read.js
+++ controllers/messages_read.js
@@ -3,6 +3,10 @@ const db_message = require('../models/message')
 module.exports = function(req, res, next) {
 	db_message
 	.find({})
+	.populate({
+		path: 'channel',
+		select: 'name'
+	})
 	.then(function(message) {
 		console.log(message);
 		res.status(200).json(message)
