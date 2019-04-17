232331aff27c71f97b93c35ffcf619bba6c0b228
Iurii Kogan <koganiurii@gmail.com>
2019-04-17 11:56:03 +0700

    updates message read to FindByID

--- controllers/message_read.js
+++ controllers/message_read.js
@@ -1,7 +1,7 @@
 const db_message = require('../models/message')
 
 module.exports = function(req, res, next) {
-	db_message.findOne({"_id" : req.params.id}).then(function(message) {
+	db_message.findById(req.params.id).then(function(message) {
 		console.log(message);
 		res.json(message)
 	})
