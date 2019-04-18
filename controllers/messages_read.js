98990ee9fc5a077819b96f958e6cc3de2f4e978f
Iurii Kogan <koganiurii@gmail.com>
2019-04-18 08:59:49 +0700

    fixed typos

--- controllers/messages_read.js
+++ controllers/messages_read.js
@@ -3,11 +3,11 @@ const db_message = require('../models/message')
 module.exports = function(req, res, next) {
 	let query = {};
 	if (req.query.channel) {
-		query = {'channel', req.query.channel}
+		query = {"channel": req.query.channel}
 	}
-	console.log(query;
+	console.log(query)
 	db_message
-	.find({'channel' : req.query.channel})
+	.find({"channel": req.query.channel})
 	.populate({
 		path: 'channel',
 		select: 'name'
